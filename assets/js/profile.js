3d7eb751e5a35c5836eef7c017011e4e5a3f2f4f
hanzhe2002 <khawhanzhe@gmail.com>
2022-06-03 02:45:06 +0800

    Improved profile image and bg upload async - fade in profile image on first load & image caching

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,6 +3,18 @@ $(document).ready(() => {
 	// var profileImgfileInput = $("#profileImgFileInput");
 	// var profileImgButton = $("#profileUploadButton");
 	// var profileImg = $(".profile-img > img");
+	//* fade in profile image on first load
+	//* if image loaded from cache, on.('load') will not be triggerd so we directly fade it in
+	if ($(".profile-img > img")[0].complete) {
+		$(".profile-img > img").fadeIn(350);
+		alert("cache load");
+	} else {
+		$(".profile-img > img").on("load", function () {
+			alert("on load");
+
+			$(this).fadeIn(350);
+		});
+	}
 
 	$("body").on("mouseover", ".profile-img", function () {
 		$("#profileUploadButton").addClass("active");
@@ -41,28 +53,34 @@ $(document).ready(() => {
 			processData: false,
 			contentType: false,
 			cache: false,
-			async: false,
 			success: function (data) {
 				if (data == "INVALID FILE UPLOAD") {
 					alert(data);
 				} else if (data != false) {
-					$(".profile-img > img")
-						.fadeOut(50, function () {
+					let profileImage = new Image();
+					profileImage.onload = function () {
+						$(".profile-img > img").fadeOut(350, function () {
+							console.log("update 3");
 							//* update profile image
-							$(".profile-img > img").attr(
-								"src",
-								base_url + "assets/img/profile_img/" + data
-							);
 							$(".profile-img > img").removeClass("default_profile");
 
-							//* update profile image for navbar mini profile as well
-							$(".profile-btn > img").attr(
-								"src",
-								base_url + "assets/img/profile_img/" + data
-							);
-							$(".profile-btn").removeClass("default_profile");
-						})
-						.fadeIn(100);
+							profileImage = $(profileImage).hide();
+							$(".profile-img > img").replaceWith(profileImage);
+							// .on("load", function () {
+							// 	$(".profile-img > img").fadeIn(1000);
+							// });
+
+							$(".profile-img > img").fadeIn(1000);
+						});
+					};
+					profileImage.src = base_url + "/assets/img/profile_img/" + data;
+
+					//* update profile image for navbar mini profile as well
+					$(".profile-btn > img").attr(
+						"src",
+						base_url + "assets/img/profile_img/" + data
+					);
+					$(".profile-btn").removeClass("default");
 
 					// $(".profile-btn").removeClass("default");
 				} else {
@@ -98,21 +116,20 @@ $(document).ready(() => {
 			processData: false,
 			contentType: false,
 			cache: false,
-			async: false,
 			success: function (data) {
 				if (data == "INVALID FILE UPLOAD") {
 					alert(data);
 				} else if (data != false) {
 					bg.css("display", "none");
-					var bgimage = new Image();
-					bgimage.src = base_url + "/assets/img/bg_img/" + data;
-					$(bgimage).on("load", function () {
+					const bgimage = new Image();
+					bgimage.onload = function () {
 						bg.css(
 							"background-image",
 							"url(" + base_url + "/assets/img/bg_img/" + data + ")"
 						);
 						bg.fadeIn(2000);
-					});
+					};
+					bgimage.src = base_url + "/assets/img/bg_img/" + data;
 				} else {
 					alert("UPLOAD UNSUCCESSFUL, PLEASE TRY AGAIN");
 				}
@@ -131,15 +148,15 @@ $(document).ready(() => {
 
 				if (bg_data.bg_state == "1") {
 					bg.css("display", "none");
-					var bgimage = new Image();
-					bgimage.src = base_url + "/assets/img/bg_img/" + bg_data.bg_img;
-					$(bgimage).on("load", function () {
+					const bgimage = new Image();
+					bgimage.onload = function () {
 						bg.css(
 							"background-image",
 							"url(" + base_url + "/assets/img/bg_img/" + bg_data.bg_img + ")"
 						);
 						bg.fadeIn(2000);
-					});
+					};
+					bgimage.src = base_url + "/assets/img/bg_img/" + bg_data.bg_img;
 				} else {
 					bg.css("background-image", "url(" + base_url + bg_data.bg_img + ")");
 					bg.fadeIn(2000);
